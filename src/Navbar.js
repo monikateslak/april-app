949dc7308f98fc6281d1ec37e5849f589b2cf9e8
Monika Mona <mona.mt.89@gmail.com>
2022-04-02 18:13:23 +0200

    navbar and users components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar () {
+    return (
+        <>
+        <div>
+            <header>NAVIGACJA</header>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/Users.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Users() {
+  return (
+    <div>
+      <section>
+        <p>
+          users
+          <br>
+            users<br>sers</br>
+          </br>
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default Users
