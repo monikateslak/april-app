ad3559fe6218df6d9d93de5efe8f6d375f616410
Monika Mona <mona.mt.89@gmail.com>
2022-04-03 16:35:37 +0200

    add navbar.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
   return (
     <>
-      <div>
+      <div id='nav-css'>
         <ul>
           <li>logo</li>
           <li>home</li>
