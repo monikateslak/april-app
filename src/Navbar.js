4e1fda8157c5317081fd22b16725f43b3df1059c
Monika Mona <mona.mt.89@gmail.com>
2022-11-02 14:32:25 +0100

    import BrowserRouter

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './Navbar.css'
+
+const Home = () => <Home></Home>
 
 function Navbar() {
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path='/' element={Home}></Route>
+      </Routes>
+    </Router>
       <div id='nav-css'>
         <ul>
           <li>logo</li>
