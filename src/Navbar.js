b3531d0a205d3bcce858a5ceea17e73407927753
Monika Mona <mona.mt.89@gmail.com>
2022-11-14 21:57:33 +0100

    changes Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './Navbar.css'
 
 const Home = () => <Home></Home>
+const Shop = () => <Shop></Shop>
+const Free = () => <Free></Free>
+const About = () => <About></About>
+const Contact = () => <Contact></Contact>
+const Join = () => <Join></Join>
 
 function Navbar() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={Home}></Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' element={Home}></Route>
+          <Route path='/shop' element={Shop}></Route>
+          <Route path='/free' element={Free} ></Route>
+          <Route path='/about' element={About}></Route>
+          <Route path='/contact' element={Contact} ></Route>
+          <Route></Route>
+        </Routes>
+      </Router>
       <div id='nav-css'>
         <ul>
           <li>logo</li>
